7c65b7e21b7741b53d67c40b7dc6e1fd2d30cff4
Logon Aluno <logonrmlocal@fiap.com.br>
2019-05-18 15:52:37 -0300

    Java fundamentals FIAP

---
+++ customer/src/main/java/br/com/fiap/customer/CustomerCreateResponse.java
@@ -0,0 +1,13 @@
+package br.com.fiap.customer;
+
+public class CustomerCreateResponse {
+    private Integer customerId;
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+}
---
+++ customer/src/main/java/br/com/fiap/customer/CustomerService.java
@@ -0,0 +1,6 @@
+package br.com.fiap.customer;
+
+public interface CustomerService {
+    CustomerResponse findById(Integer id);
+    CustomerCreateResponse create(CustomerRequest customerRequest);
+}
